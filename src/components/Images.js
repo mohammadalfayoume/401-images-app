5584da3cb72b601d895c2cca91ab97375176d2d4
Mohammad Alfayoume <mabed3819@gmail.com>
2022-12-30 13:51:57 +0200

    added dislike and some styling

--- src/components/Images.js
+++ src/components/Images.js
@@ -2,27 +2,26 @@ import React, {useState} from 'react'
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
-// import Row from 'react-bootstrap/Row';
 
 const Images = (props) => {
     const [likes, setLikes]= useState(0)
-    function likesHandler(){
-        setLikes(likes+1)
-        props.totalLikesHandler()
+    function likesHandler(c){
+        if (c==="i"){
+          setLikes(likes+1)
+          props.totalLikesHandler(c)
+        }
+        else {
+          if(likes===0){
+            setLikes(0)
+          }
+          else{
+            setLikes(likes-1)
+            props.totalLikesHandler(c)
+          }
+        }  
     }
   return (
     <>
-    {/* <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={props.img} alt={props.title} />
-      <Card.Body>
-        <Card.Title>Title: {props.title}</Card.Title>
-        <Card.Text>
-            <h5>ID: {props.id}</h5>
-            <h4>likes: {likes}</h4>
-        </Card.Text>
-        <Button variant="primary" onClick={likesHandler} >Add Like</Button>
-      </Card.Body>
-    </Card> */}
         <Card>
             <Col>
                 <Card.Img variant="top" src={props.img} alt={props.title} />
@@ -32,7 +31,10 @@ const Images = (props) => {
                     <h5>ID: {props.id}</h5>
                     <h4>likes: {likes}</h4>
                 </Card.Text>
-                <Button variant="primary" onClick={likesHandler} >Add Like</Button>
+                <div style={{display:'flex', flexDirection:'row', gap:'1rem'}}>
+                <Button variant="primary" onClick={()=>likesHandler("i")} >Like</Button>
+                <Button variant="primary" onClick={()=>likesHandler("d")} >dislike</Button>
+                </div>
                 </Card.Body>
             </Col>
         </Card>
--- src/components/Parent.js
+++ src/components/Parent.js
@@ -6,12 +6,18 @@ import Row from 'react-bootstrap/Row';
 
 const Parent = () => {
     const [totalLikes, setTotalLikes]= useState(0)
-    function totalLikesHandler(){
+    function totalLikesHandler(c){
+      if (c==="i"){
         setTotalLikes(totalLikes+1)
+      }
+      else{
+        setTotalLikes(totalLikes-1)
+      }
     }
+    
   return (
     <>
-    <h1 style={{textAlign:'center',backgroundColor:'yellowgreen'}}>Total likes: {totalLikes}</h1>
+    <h1 style={{textAlign:'center',backgroundColor:'yellowgreen', margin:'1rem 40%',width:'20%',border:'solid black 1px', borderRadius:'0.5rem'}}>Total likes: {totalLikes}</h1>
     <Row xs={1} md={3} className="g-4">
     {data.map((card,idx)=>{
         return(
